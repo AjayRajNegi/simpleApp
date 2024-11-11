b41aa41f0c33ae4156d4864b9666e7d3a947a122
Ajay <ajayrajnegi111@gmail.com>
2024-11-11 22:25:03 +0530

    t3

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 resource "aws_instance" "servernode" {
     ami = "ami-08eb150f611ca277f"
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
     key_name = aws_key_pair.deployer.key_name
     vpc_security_group_ids = [aws_security_group.maingroup.id]
     iam_instance_profile = aws_iam_instance_profile.ec2-profile.name
